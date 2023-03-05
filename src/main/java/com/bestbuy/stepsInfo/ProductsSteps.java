3c78e9a9230bc7b66384ed138a56b117dd88fa6d
kirtz7 <kirtan77795@googlemail.com>
2023-03-05 10:24:17 +0000

    1.1

---
+++ src/main/java/com/bestbuy/stepsInfo/ProductsSteps.java
@@ -0,0 +1,4 @@
+package com.bestbuy.stepsInfo;
+
+public class ProductsSteps {
+}
--- src/test/java/com/bestbuy/stepstests/CategoriesCRUDSteps.java
+++ src/test/java/com/bestbuy/stepstests/CategoriesCRUDSteps.java
@@ -1,4 +1,21 @@
 package com.bestbuy.stepstests;
 
-public class CategoriesCRUDSteps {
+
+import com.bestbuy.testbase.TestBase;
+import com.bestbuy.utils.TestUtils;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Steps;
+import org.junit.runner.RunWith;
+
+@RunWith(SerenityRunner.class)
+public class CategoriesCRUDSteps extends TestBase {
+
+    static String name = "TestCategory" + TestUtils.getRandomValue();
+    static String category = "10" + TestUtils.getRandomValue();
+    static Object categoryId;
+
+    @Steps
+    C
+
+
 }
